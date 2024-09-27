ca4edfcf125b50423477aa33cd49783961bd52f3
Dieye_code <moustaphadieye96@gmail.com>
2024-09-27 09:45:37 +0000

    change home page

--- resources/js/components/Home.jsx
+++ resources/js/components/Home.jsx
@@ -44,7 +44,7 @@ function Home() {
 
   return (
     <>
-      <div className="row row-cols-1 row-cols-md-3 row-cols-lg-3 row-cols-xl-4">
+      <div className="row row-cols-1 row-cols-sm-2 row-cols-md-3 row-cols-lg-3 row-cols-xl-4">
         <div className="col">
           <div className="card radius-10">
             <div className="card-body">
